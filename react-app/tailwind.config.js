73050f5911476aad4f5ef460bb166ebd0cf9c582
21DP1RRoze <11drrozenvalds@rvt.lv>
2024-09-09 10:24:42 +0300

    initial react commit

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "Poppins",
+        "playwrite-cuba": "Playwrite CU",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
