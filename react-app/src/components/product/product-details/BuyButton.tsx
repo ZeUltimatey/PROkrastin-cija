a00661a62b17aaf6a593c3aaf19c745c816881ea
Monta Montvida <monta.montvida9@gmail.com>
2024-09-14 22:44:56 +0300

    Buttons for product quantity, breadcrumbs

--- react-app/src/components/product/product-details/BuyButton.tsx
+++ react-app/src/components/product/product-details/BuyButton.tsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 export const BuyButton = () => {
+  const [count, setCount] = useState(1);
+
+  const countPluss = () => {
+    setCount(count + 1);
+  };
+
+  const countMinus = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="mt-6 text-left">
+    <div className="mt-6 text-left flex items-center">
       <button className="bg-[#C59D82] text-white px-6 py-3 text-lg rounded-xl shadow hover:bg-[#b38b6f]">
         Pievienot grozam
       </button>
+
+      <div className="ml-4 flex items-center">
+        <button
+          onClick={countMinus}
+          className="bg-[#C59D82] text-black font-bold px-3 py-2 rounded-l-xl hover:bg-[#b38b6f]"
+        >
+          -
+        </button>
+
+        <div className="bg-white border font-semibold px-4 py-2 text-lg text-center">
+          {count}
+        </div>
+
+        <button
+          onClick={countPluss}
+          className="bg-[#C59D82] text-black font-bold px-3 py-2 rounded-r-xl hover:bg-[#b38b6f]"
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 };
--- react-app/src/components/product/product-details/ProductDetails.tsx
+++ react-app/src/components/product/product-details/ProductDetails.tsx
@@ -1,13 +1,12 @@
 import { ProductImage } from "./ProductImage";
 import { ProductInfo } from "./ProductInfo";
 import { ProductSpecification } from "./ProductSpecification";
-
+import { Breadcrumbs } from "./Breadcrumbs";
 
 export const ProductDetails = () => {
   return (
     <div className="h-screen bg-[#EDEAE1] rounded-t px-4 sm:px-8 lg:px-12 mt-1">
-      <div className="text-base lg:text-xl mt-4 lg:mt-2 font-semibold">Preces / Barība</div>
-
+      <Breadcrumbs />
       <div className="mt-6 flex flex-col lg:flex-row lg:space-x-6">
         <ProductImage />
         <ProductInfo />
