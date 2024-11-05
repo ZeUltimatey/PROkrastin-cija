725dc57136ab2f5c673ce98a09cac6e8c7748ccf
21DP1KKoka <103510962+21DP1KKoka@users.noreply.github.com>
2024-11-05 13:28:12 +0200

    Revert "Montaa dev"

--- src/react-app/src/App.tsx
+++ src/react-app/src/App.tsx
@@ -25,78 +25,44 @@ import { Statistics } from "./components/panel/admin-panel/Statistics";
 import { ConfirmationProvider } from "./components/universal/Confirmation";
 import { Cats } from "./components/panel/admin-panel/Cats";
 import { Breeds } from "./components/panel/admin-panel/Breeds";
-import { ProfileSettings } from "./components/instruction/ProfileSettings";
-import { BuyInfo } from "./components/instruction/BuyInfo";
-import { ContactUse } from "./components/instruction/ContactUse";
-import { EncyclopediaUse } from "./components/instruction/EncyclopediaUse";
-import { ProfileEdit } from "./components/instruction/ProfileEdit";
-import { OrderHistory } from "./components/instruction/OrderHistory";
-import { SavedAddresses } from "./components/instruction/SavedAddresses";
-import { PayMethods } from "./components/instruction/PayMethods";
-import { BrowseInfo } from "./components/instruction/BrowseInfo";
-import { BasketInfo } from "./components/instruction/BasketInfo";
 
 function App() {
   return (
     <BrowserRouter>
       <ToastProvider>
-        <CartProvider>
-          <Routes>
-            <Route path="/" element={<GeneralLayout />}>
-              <Route index element={<Homepage />} />
-              <Route path="/product/:productId" element={<ProductView />} />
-              <Route path="/products" element={<Catalog />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/policy" element={<Policy />} />
-              <Route path="/contact" element={<ContactInfo />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/reviews" element={<Reviews />} />
-              <Route path="/receipt" element={<Receipt />} />
-              <Route path="/breeds" element={<BreedCatalog />} />
-              <Route path="/breed/:breedId" element={<BreedDetails />} />
-              <Route
-                path="/instruction/encyclopediause"
-                element={<EncyclopediaUse />}
-              />
-              <Route
-                path="/instruction/profilesettings"
-                element={<ProfileSettings />}
-              />
-              <Route path="/instruction/basketinfo" element={<BasketInfo />} />
-              <Route path="/instruction/browseinfo" element={<BrowseInfo />} />
-
-              <Route path="/instruction/buyinfo" element={<BuyInfo />} />
-              <Route path="/instruction/contactuse" element={<ContactUse />} />
-              <Route
-                path="/instruction/profileedit"
-                element={<ProfileEdit />}
-              />
-              <Route
-                path="/instruction/orderhistory"
-                element={<OrderHistory />}
-              />
-              <Route
-                path="/instruction/savedaddresses"
-                element={<SavedAddresses />}
-              />
-              <Route path="/instruction/paymethods" element={<PayMethods />} />
-            </Route>
-            <Route path="/panel" element={<PanelLayout />}>
-              <Route path="/panel/statistics" element={<Statistics />} />
-              <Route path="/panel/users" element={<Users />} />
-              <Route path="/panel/orders" element={<Orders />} />
-              <Route path="/panel/products" element={<Products />} />
-              <Route path="/panel/cats" element={<Cats />} />
-              <Route path="/panel/breeds" element={<Breeds />} />
-              <Route path="/panel/encyclopedia" element={<Encyclopedia />} />
-            </Route>
-            <Route path="/auth" element={<AuthLayout />}>
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-            </Route>
-            <Route path="/catalog/cats" element={<Homepage />} />
-          </Routes>
-        </CartProvider>
+        <ConfirmationProvider>
+          <CartProvider>
+            <Routes>
+              <Route path="/" element={<GeneralLayout />}>
+                <Route index element={<Homepage />} />
+                <Route path="/product/:productId" element={<ProductView />} />
+                <Route path="/products" element={<Catalog />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/policy" element={<Policy />} />
+                <Route path="/contact" element={<ContactInfo />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/reviews" element={<Reviews />} />
+                <Route path="/receipt" element={<Receipt />} />
+                <Route path="/breeds" element={<BreedCatalog />} />
+                <Route path="/breed/:breedId" element={<BreedDetails />} />
+              </Route>
+              <Route path="/panel" element={<PanelLayout />}>
+                <Route path="/panel/statistics" element={<Statistics />} />
+                <Route path="/panel/users" element={<Users />} />
+                <Route path="/panel/orders" element={<Orders />} />
+                <Route path="/panel/products" element={<Products />} />
+                <Route path="/panel/cats" element={<Cats />} />
+                <Route path="/panel/breeds" element={<Breeds />} />
+                <Route path="/panel/encyclopedia" element={<Encyclopedia />} />
+              </Route>
+              <Route path="/auth" element={<AuthLayout />}>
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+              </Route>
+              <Route path="/catalog/cats" element={<Homepage />} />
+            </Routes>
+          </CartProvider>
+        </ConfirmationProvider>
       </ToastProvider>
     </BrowserRouter>
   );
