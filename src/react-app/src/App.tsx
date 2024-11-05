bd86e442cd0333ad7663fd71acf020c6772daa82
21DP1KKoka <103510962+21DP1KKoka@users.noreply.github.com>
2024-11-05 13:10:17 +0200

    Update App.tsx FIX 2 (HOPEFULLY(a tiny bit less clueless BE dev)

--- src/react-app/src/App.tsx
+++ src/react-app/src/App.tsx
@@ -45,14 +45,14 @@ function App() {
             <Route path="/" element={<GeneralLayout />}>
               <Route index element={<Homepage />} />
               <Route path="/product/:productId" element={<ProductView />} />
-              <Route path="/catalog" element={<Catalog />} />
+              <Route path="/products" element={<Catalog />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/policy" element={<Policy />} />
               <Route path="/contact" element={<ContactInfo />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/reviews" element={<Reviews />} />
               <Route path="/receipt" element={<Receipt />} />
-              <Route path="/breeds" element={<Breeds />} />
+              <Route path="/breeds" element={<BreedCatalog />} />
               <Route path="/breed/:breedId" element={<BreedDetails />} />
               <Route
                 path="/instruction/encyclopediause"
@@ -86,6 +86,8 @@ function App() {
               <Route path="/panel/users" element={<Users />} />
               <Route path="/panel/orders" element={<Orders />} />
               <Route path="/panel/products" element={<Products />} />
+              <Route path="/panel/cats" element={<Cats />} />
+              <Route path="/panel/breeds" element={<Breeds />} />
               <Route path="/panel/encyclopedia" element={<Encyclopedia />} />
             </Route>
             <Route path="/auth" element={<AuthLayout />}>
