6bc317d18d95be8a4f63bd8146203b466bf7cceb
Matq <matissbozko@gmail.com>
2024-11-30 19:06:39 +0200

    Preferences for lowest prices and available stock on be

--- src/laravel-app/app/Http/Controllers/ProductController.php
+++ src/laravel-app/app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductRequest;
+use App\Http\Requests\ProductSearchRequest;
 use App\Http\Resources\UserResource;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
